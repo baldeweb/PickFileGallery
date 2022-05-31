93afb6ab89a356f06d4cda4361efcb706b27c1d5
Desenv - Wallace Baldenebre Da Silva <wallace.silva@bancodaycoval.com.br>
2022-05-31 11:07:17 -0300

    removing not used methods

--- app/src/main/java/com/wallace/pickfilegallery/LogUtils.kt
+++ app/src/main/java/com/wallace/pickfilegallery/LogUtils.kt
@@ -1,28 +1,10 @@
 package com.wallace.pickfilegallery
 
-import android.app.Activity
 import android.util.Log
-import androidx.fragment.app.Fragment
 
 object LogUtils {
     const val TAG_LOG = "LOG"
-    fun Activity.logD(text: String) {
-        Log.d(TAG_LOG, "[${this.javaClass.simpleName}] - $text")
-    }
-
-    fun Fragment.logD(text: String) {
-        Log.d(TAG_LOG, "[${this.javaClass.simpleName}] - $text")
-    }
-
     fun logD(text: String) {
         Log.d(TAG_LOG, "[${this.javaClass.simpleName}] - $text")
-    }
-
-    fun logE(text: String) {
-        Log.e(TAG_LOG, text)
-    }
-
-    fun logV(text: String) {
-        Log.v(TAG_LOG, text)
     }
 }
